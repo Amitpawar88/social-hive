99496d0b1074eb16d4641558428fe59dcbfdbf41
amitpawar <amitpawar7475@gmail.com>
2024-05-18 23:15:40 +0530

    Client,

---
+++ src/components/Feed.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import CreatePost from './CreatePost.js'
+import Tweet from './Tweet.js'
+
+
+const Feed = () => {
+  return (
+    <div className='w-[50%] border border-gray-200'>
+      <div>
+        <CreatePost />
+        <Tweet />
+      </div>
+    </div>
+  )
+}
+
+export default Feed
---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import LeftSidebar from "./LeftSidebar";
+import RightSidebar from "./RightSidebar";
+import { Outlet } from "react-router-dom";
+
+export const Home = () => {
+  return (
+    <div>
+      <div className="flex justify-between w-[80%] mx-auto">
+        <LeftSidebar />
+        <Outlet />
+        <RightSidebar />
+      </div>
+    </div>
+  );
+};

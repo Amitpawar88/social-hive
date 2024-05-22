81c37e8faaa744f1112591e500559b46887bc13e
amitpawar <amitpawar7475@gmail.com>
2024-05-22 15:36:06 +0530

    add persist store, logout functionality

--- src/components/LeftSidebar.js
+++ src/components/LeftSidebar.js
@@ -5,11 +5,29 @@ import { IoIosNotificationsOutline } from "react-icons/io";
 import { CiUser } from "react-icons/ci";
 import { CiBookmark } from "react-icons/ci";
 import { AiOutlineLogout } from "react-icons/ai";
-import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import axios from "axios";
+import { USER_API_END_POINT } from "../utils/constant";
+import toast from "react-hot-toast";
+import { getMyProfile, getOtherUsers, getUser } from "../redux/userSlice";
 
 const LeftSidebar = () => {
-  const { user} = useSelector((store) => store.user);
+  const { user } = useSelector((store) => store.user);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const logoutHandler = async () => {
+    try {
+      const res = await axios.get(`${USER_API_END_POINT}/logout`);
+      dispatch(getUser(null));
+      dispatch(getOtherUsers(null));
+      dispatch(getMyProfile(null));
+      navigate("/login");
+      toast.success(res.data.message);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="w-[20%]">
@@ -22,7 +40,9 @@ const LeftSidebar = () => {
             alt="twitter-logo"
           />
         </div>
-        <Link to='/'
+        <div className="my-4">
+          <Link
+            to="/"
             className="flex items-center my-2 px-4 py-2 hover:bg-gray-200 hover:cursor-pointer rounded-full"
           >
             <div>
@@ -42,7 +62,8 @@ const LeftSidebar = () => {
             </div>
             <h1 className="font-bold text-lg ml-2">Notifications</h1>
           </div>
-          <Link to={`/profile/${user?._id}`}
+          <Link
+            to={`/profile/${user?._id}`}
             className="flex items-center my-2 px-4 py-2 hover:bg-gray-200 hover:cursor-pointer rounded-full"
           >
             <div>
@@ -57,6 +78,7 @@ const LeftSidebar = () => {
             <h1 className="font-bold text-lg ml-2">Bookmarks</h1>
           </div>
           <div
+            onClick={logoutHandler}
             className="flex items-center my-2 px-4 py-2 hover:bg-gray-200 hover:cursor-pointer rounded-full"
           >
             <div>
@@ -69,6 +91,7 @@ const LeftSidebar = () => {
           </button>
         </div>
       </div>
+    </div>
   );
 };
 
